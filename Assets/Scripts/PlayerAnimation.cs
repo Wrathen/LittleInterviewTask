c81387f0b5ec0c8252ef874e8a9a7f4b2c5b0039
Wrathen <frostiarn@gmail.com>
2022-11-08 05:51:11 +0300

    Player Animations

--- Assets/Scripts/PlayerAnimation.cs
+++ Assets/Scripts/PlayerAnimation.cs
@@ -1,18 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerAnimation : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+[RequireComponent(typeof(Animator), typeof(PlayerMovement))]
+public class PlayerAnimation : MonoBehaviour {
+    private Animator animator;
+    private PlayerMovement playerMovement;
+
+    void Awake() {
+        animator = GetComponent<Animator>();
+        playerMovement = GetComponent<PlayerMovement>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // I think this setup works much better than multiple booleans
+    void Update() { animator.SetInteger("MoveDirection", GetMoveDirection()); }
+
+    int GetMoveDirection() { return IsMovingUp() ? 1 : IsMovingDown() ? 2 : IsMovingRight() ? 3 : IsMovingLeft() ? 4 : 0; }
+    bool IsMovingLeft() { return playerMovement.velocity.x < 0.0f; }
+    bool IsMovingRight() { return playerMovement.velocity.x > 0.0f; }
+    bool IsMovingUp() { return playerMovement.velocity.y > 0.0f; }
+    bool IsMovingDown() { return playerMovement.velocity.y < 0.0f; }
 }
