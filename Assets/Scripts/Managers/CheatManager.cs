204892608d6a42a067c3dfa7fffc5787b97f366e
Wrathen <frostiarn@gmail.com>
2022-11-09 09:56:12 +0300

    Build Added

--- Assets/Scripts/Managers/CheatManager.cs
+++ Assets/Scripts/Managers/CheatManager.cs
@@ -3,22 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CheatManager : MonoBehaviour {
-    public GameObject helmet;
-    public GameObject gloves;
-    public GameObject pants;
-    public GameObject shoes;
     public static bool cheatsEnabled = true;
 
     void Update() {
         if (Input.GetKeyDown(KeyCode.Alpha1))
             GameManager.AddMoney(5000);
-        if (Input.GetKeyDown(KeyCode.Alpha2))
-            helmet.SetActive(!helmet.activeSelf);
-        if (Input.GetKeyDown(KeyCode.Alpha3))
-            gloves.SetActive(!gloves.activeSelf);
-        if (Input.GetKeyDown(KeyCode.Alpha4))
-            pants.SetActive(!pants.activeSelf);
-        if (Input.GetKeyDown(KeyCode.Alpha5))
-            shoes.SetActive(!shoes.activeSelf);
     }
 }
